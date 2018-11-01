292f2fdcd6cc7d4fc4750bd6d2e73eb1eeab57cd
ming.tang <tangm@xiaopeng.com>
2018-11-01 20:50:05 +0800

    [ Modify      ] 修改编译脚本，革命尚未成功 [ Module      ] MusicRadio [ Description ] [ Reference   ] NA [ Company     ] 广州小鹏 [ Team        ] 音乐应用组

--- app/src/main/cpp/xp-ffmpeg.cpp
+++ app/src/main/cpp/xp-ffmpeg.cpp
@@ -1,10 +1,16 @@
 #include <jni.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+extern "C" {
 
 JNIEXPORT jstring JNICALL
 Java_com_xiaopeng_ffmpegstudy_FfmpegActivity_showCodec(JNIEnv *env, jobject instance) {
 
     // TODO
+//    av_register_all();
 
 
     return (jstring) "1";
+}
 }
--- app/src/main/java/com/xiaopeng/ffmpegstudy/FfmpegActivity.java
+++ app/src/main/java/com/xiaopeng/ffmpegstudy/FfmpegActivity.java
@@ -3,6 +3,7 @@ package com.xiaopeng.ffmpegstudy;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * @author tony
@@ -10,10 +11,13 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class FfmpegActivity extends AppCompatActivity {
+    private TextView mTvCodec;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ffmpeg);
+        mTvCodec= (TextView) findViewById(R.id.tv_codec);
+        mTvCodec.setText(showCodec());
     }
     public native String showCodec();
     static {
