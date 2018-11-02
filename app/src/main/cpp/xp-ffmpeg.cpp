ca3cd3c24d46ffb35c8bc086a2874c979ee54db0
ming.tang <tangm@xiaopeng.com>
2018-11-02 14:13:32 +0800

    [ Modify      ] 解决ffmpeg的编译问题 [ Module      ] MusicRadio [ Description ] [ Reference   ] NA [ Company     ] 广州小鹏 [ Team        ] 音乐应用组

--- app/src/main/cpp/xp-ffmpeg.cpp
+++ app/src/main/cpp/xp-ffmpeg.cpp
@@ -1,16 +1,35 @@
 #include <jni.h>
+
+extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavfilter/avfilter.h>
-extern "C" {
-
 JNIEXPORT jstring JNICALL
 Java_com_xiaopeng_ffmpegstudy_FfmpegActivity_showCodec(JNIEnv *env, jobject instance) {
 
-    // TODO
-//    av_register_all();
-
-
-    return (jstring) "1";
+    char info[40000] = {0};
+    av_register_all();
+    AVCodec *c_temp = av_codec_next(NULL);
+    while (c_temp != NULL) {
+        if (c_temp->decode != NULL) {
+            sprintf(info, "%sdecode:", info);
+        } else {
+            sprintf(info, "%sencode:", info);
+        }
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s(video):", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s(audio):", info);
+                break;
+            default:
+                sprintf(info, "%s(other):", info);
+                break;
+        }
+        sprintf(info, "%s[%10s]\n", info, c_temp->name);
+        c_temp = c_temp->next;
+    }
+    return env->NewStringUTF(info);
 }
 }
